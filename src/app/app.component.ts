ca2b92912351844f908e55858f7896aaff391299
Leonardo Pinheiro <leoprocha100@gmail.com>
2021-07-19 22:14:37 -0300

    feat (app): create example of Plug Checkout integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
 
+import {
+  PlugCheckoutOneShotSuccess,
+  PlugCheckoutOneShotError,
+} from './app.types';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  installmentsConfig = { show: true, quantity: 2 };
+  customFormStyleClasses = { submitButton: 'custom-submit-button' };
+
+  handlePaymentSuccess(data: PlugCheckoutOneShotSuccess) {
+    console.log(data);
+  }
+
+  handlePaymentFailed(error: PlugCheckoutOneShotError) {
+    console.log(error);
+  }
+}
